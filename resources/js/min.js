477a8a92a8857e05cc0c4f82c7c753fb259dd801
Feifx <32243939+faysoserious@users.noreply.github.com>
2020-10-15 16:35:46 +0200

    adjust header and footer

---
+++ resources/js/min.js
@@ -0,0 +1,8 @@
+
+function myFunction(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
